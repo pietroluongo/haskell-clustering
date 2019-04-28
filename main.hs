27793a7d31d14ef9816d3d231466518d4804db1b
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2019-04-28 23:04:06 +0000

    Added planning to main file

--- main.hs
+++ main.hs
@@ -1,10 +1,8 @@
 import Algebra.Point
 import Data.List
-stuffs _ [] = []
-stuffs [] _ = []
-stuffs (x:xs) (y:ys) = [x+y]:stuffs xs ys
 
---stuffsDim :: (Num a) => [[a]] -> [a]
-stuffsDim xss = map (sum) txss
-    where
-        txss = transpose xss
+-- read k.txt
+-- read entrada.txt
+-- do grouping algorithm
+-- write sse value to result.txt
+-- write output to saida.txt
