c0a26a03969005e1e73d784f64a8aa2631a7e4a2
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2019-04-30 05:36:44 +0000

    WIP: getPoints implementation

--- main.hs
+++ main.hs
@@ -1,6 +1,16 @@
 import Algebra.Point
 import Data.List
 import Data.Function
+import Utils
+
+-- DEBUGGING ---------------------------------------------
+
+__dataset = [[3, 2, 1], [1, 2, 3], [4, 5, 6], [7, 8, 9], [9, 8, 7]]
+
+__dataset_fst_point = getFirstPoint __dataset
+
+----------------------------------------------------------
+
 
 -- Function that finds the first point of the dataset
 -- Parameters:
@@ -8,7 +18,7 @@ import Data.Function
 -- Output:
 --      point: The point that should be the first one to be evaluated
 -- The next function to be called should be getSecondPoint, passing the dataset and the output of this function as parameters
-getFirstPoint points = head minPoint
+getFirstPoint points = fst $ head minPoint
     where
         cpoints = map Point points
         zipped = zip (points) (map (coordSum) cpoints)
@@ -24,7 +34,7 @@ getFirstPoint points = head minPoint
 --      initial: [Int], the initial point of the dataset (calculated on getFirstPoint)
 -- Output:
 --      point: The second point of the Dataset
-getSecondPoint points initial = maxDistPoint
+getSecondPoint points initial = fst $ head maxDistPoint
     where
         cpoints = map Point points
         cpoint = Point initial
@@ -42,13 +52,23 @@ getSecondPoint points initial = maxDistPoint
 --      [Points]: Centroids from groups
 getCentroids points k
     | k == 0 = []
-    | otherwise = closest:getCentroids (remove points $ coords $ fst closest) (k-1)
+    | otherwise = (coords $ fst closest):getCentroids (remove points $ coords $ fst closest) (k-1)
     where
         pPoints = map Point points
         cent = centroid pPoints
         dists = map (dist cent) pPoints
         sorted = sortBy (compare `on` snd) $ zip pPoints dists
         closest = head sorted
+
+getPoints dataset k
+    | k == 0 = []
+    | otherwise = firstPoint:secondPoint:getCentroids filteredDataset (k-2)
+    where
+        firstPoint = getFirstPoint dataset
+        secondPoint = getSecondPoint dataset firstPoint
+        filteredDataset' = remove dataset firstPoint
+        filteredDataset = remove filteredDataset' secondPoint
+
 
 -- read k.txt
 -- read entrada.txt
