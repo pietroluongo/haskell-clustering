4ee85fd85e63bb0d63137aec8a809f1652391629
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2019-04-30 14:16:02 -0300

    Improved code organization
    Relocated functions from main.hs to Point.hs in order to improve code organization.

--- Algebra/Point.hs
+++ Algebra/Point.hs
@@ -3,10 +3,13 @@ module Algebra.Point (
     dist,
     centroid,
     sse,
-    coordSum
+    coordSum,
+    getPoints
 ) where
 
 import Data.List
+import Utils
+import Data.Function
 
 -- Abstract type "Point"
 -- Fields:
@@ -56,3 +59,71 @@ sse points centroid = foldl (\acc x -> acc + (dist x centroid)^2) 0 points
 --      Float: Sum of the coordinates of the point
 coordSum :: Point -> Float
 coordSum point = sum $ coords point
+
+-------------------------------------------------------------------------------------------------------------------------------------------
+-- Project specific stuff
+-------------------------------------------------------------------------------------------------------------------------------------------
+
+-- Function that finds the first point of the dataset
+-- Parameters:
+--      points: [[Int]], the dataset where each element on the list represents one point on the dataset
+-- Output:
+--      point: The point that should be the first one to be evaluated
+-- The next function to be called should be getSecondPoint, passing the dataset and the output of this function as parameters
+getFirstPoint points = fst $ head minPoint
+    where
+        cpoints = map Point points
+        zipped = zip (points) (map (coordSum) cpoints)
+        sorted = sortBy (compare `on` snd) zipped
+        minSum = snd $ head sorted
+        pointsWithMinSum = [x | x <- sorted, (snd x) == minSum]
+        minPoint = sortBy (compare `on` fst) pointsWithMinSum
+
+
+-- Function that finds the second point of the dataset
+-- Parameters:
+--      points: [[Int]], the dataset where each element on the list represents one point on the dataset
+--      initial: [Int], the initial point of the dataset (calculated on getFirstPoint)
+-- Output:
+--      point: The second point of the Dataset
+getSecondPoint points initial = fst $ head maxDistPoint
+    where
+        cpoints = map Point points
+        cpoint = Point initial
+        zipped = zip (points) (map (dist cpoint) cpoints)
+        sorted = drop 1 $ sortBy (compare `on` snd) zipped
+        maxDist = snd $ last sorted
+        pointsWithMaxDist = [x | x <- sorted, (snd x) == maxDist]
+        maxDistPoint = sortBy (compare `on` fst) pointsWithMaxDist
+
+-- Function that sets K points as the centroids of the groups, but without considering the first and second points specifically. Called by getPoints
+-- Parameters:
+--      points: [[Int]] points to be analyzed
+--      k: Int, # of points to be picked
+-- Output:
+--      [Points]: Centroids from groups
+getCentroids points k
+    | k == 0 = []
+    | otherwise = (coords $ fst closest):getCentroids (remove points $ coords $ fst closest) (k-1)
+    where
+        pPoints = map Point points
+        cent = centroid pPoints
+        dists = map (dist cent) pPoints
+        sorted = sortBy (compare `on` snd) $ zip pPoints dists
+        closest = head sorted
+
+-- Function that sets K points as the centroids of the groups. This is the main grouping function
+-- Parameters:
+--     dataset: [[Int]] points to be analyzed
+--     k: Int, # of points
+-- Output:
+--     [Points]: Centroids from all groups 
+getPoints dataset k
+    | k == 0 = []
+    | otherwise = firstPoint:secondPoint:getCentroids filteredDataset (k-2)
+    where
+        firstPoint = getFirstPoint dataset
+        secondPoint = getSecondPoint dataset firstPoint
+        filteredDataset' = remove dataset firstPoint
+        filteredDataset = remove filteredDataset' secondPoint
+
--- main.hs
+++ main.hs
@@ -5,76 +5,11 @@ import Utils
 
 -- DEBUGGING ---------------------------------------------
 
-__dataset = [[3, 2, 1], [1, 2, 3], [4, 5, 6], [7, 8, 9], [9, 8, 7]]
+--__dataset = [[3, 2, 1], [1, 2, 3], [4, 5, 6], [7, 8, 9], [9, 8, 7]]
 
-__dataset_fst_point = getFirstPoint __dataset
+--__dataset_fst_point = getFirstPoint __dataset
 
 ----------------------------------------------------------
-
-
--- Function that finds the first point of the dataset
--- Parameters:
---      points: [[Int]], the dataset where each element on the list represents one point on the dataset
--- Output:
---      point: The point that should be the first one to be evaluated
--- The next function to be called should be getSecondPoint, passing the dataset and the output of this function as parameters
-getFirstPoint points = fst $ head minPoint
-    where
-        cpoints = map Point points
-        zipped = zip (points) (map (coordSum) cpoints)
-        sorted = sortBy (compare `on` snd) zipped
-        minSum = snd $ head sorted
-        pointsWithMinSum = [x | x <- sorted, (snd x) == minSum]
-        minPoint = sortBy (compare `on` fst) pointsWithMinSum
-
-
--- Function that finds the second point of the dataset
--- Parameters:
---      points: [[Int]], the dataset where each element on the list represents one point on the dataset
---      initial: [Int], the initial point of the dataset (calculated on getFirstPoint)
--- Output:
---      point: The second point of the Dataset
-getSecondPoint points initial = fst $ head maxDistPoint
-    where
-        cpoints = map Point points
-        cpoint = Point initial
-        zipped = zip (points) (map (dist cpoint) cpoints)
-        sorted = drop 1 $ sortBy (compare `on` snd) zipped
-        maxDist = snd $ last sorted
-        pointsWithMaxDist = [x | x <- sorted, (snd x) == maxDist]
-        maxDistPoint = sortBy (compare `on` fst) pointsWithMaxDist
-
--- Function that sets K points as the centroids of the groups, but without considering the first and second points specifically. Called by getPoints
--- Parameters:
---      points: [[Int]] points to be analyzed
---      k: Int, # of points to be picked
--- Output:
---      [Points]: Centroids from groups
-getCentroids points k
-    | k == 0 = []
-    | otherwise = (coords $ fst closest):getCentroids (remove points $ coords $ fst closest) (k-1)
-    where
-        pPoints = map Point points
-        cent = centroid pPoints
-        dists = map (dist cent) pPoints
-        sorted = sortBy (compare `on` snd) $ zip pPoints dists
-        closest = head sorted
-
--- Function that sets K points as the centroids of the groups. This is the main grouping function
--- Parameters:
---     dataset: [[Int]] points to be analyzed
---     k: Int, # of points
--- Output:
---     [Points]: Centroids from all groups 
-getPoints dataset k
-    | k == 0 = []
-    | otherwise = firstPoint:secondPoint:getCentroids filteredDataset (k-2)
-    where
-        firstPoint = getFirstPoint dataset
-        secondPoint = getSecondPoint dataset firstPoint
-        filteredDataset' = remove dataset firstPoint
-        filteredDataset = remove filteredDataset' secondPoint
-
 
 -- read k.txt
 -- read entrada.txt
