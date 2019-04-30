130ab1858d6c418c11de1d43279d3871f07f2e58
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2019-04-30 03:19:49 +0000

    Initial implementation of the getFirstPoint function. Will probably be relocated to another file in order to improve code organization.

--- main.hs
+++ main.hs
@@ -2,11 +2,22 @@ import Algebra.Point
 import Data.List
 import Data.Function
 
-
-getFirstPoint points = head $ sortBy (compare `on` snd) zipped
+-- Function that finds the first point of the dataset
+-- Parameters:
+--      points: [[Int]], the dataset where each element on the list represents one point on the dataset
+-- Output:
+--      point: The point that should be the first one to be evaluated
+-- The next function to be called should be getSecondPoint, passing the dataset and the output of this function as parameters
+getFirstPoint points = head minPoint
     where
         cpoints = map Point points
         zipped = zip (points) (map (coordSum) cpoints)
+        sorted = sortBy (compare `on` snd) zipped
+        minSum = snd $ head sorted
+        pointsWithMinSum = [x | x <- sorted, (snd x) == minSum]
+        minPoint = sortBy (compare `on` fst) pointsWithMinSum
+
+
 -- read k.txt
 -- read entrada.txt
 -- do grouping algorithm
