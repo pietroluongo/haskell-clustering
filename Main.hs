48178d67f23d5e150ccc76bd03b0a541f76d8c24
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2019-05-05 05:56:04 -0300

    WIP - Proper I/O implementation

--- Main.hs
+++ Main.hs
@@ -3,7 +3,7 @@ import Data.List
 import Data.Function
 import Utils
 import Algebra.Group
-
+import System.IO
 -- DEBUGGING ---------------------------------------------
 
 __dataset = [[3.0, 2.0, 1.0], [1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0], [9.0, 8.0, 7.0]]
@@ -15,6 +15,8 @@ __input_file_points = "./testcases/entrada.txt"
 __output_file_groups = "./output/saida.txt"
 __output_file_res = "./output/result.txt"
 
+__CONST_MAX_IT = 100
+
 ----------------------------------------------------------
 
 -- read k.txt
@@ -24,4 +26,10 @@ __output_file_res = "./output/result.txt"
 -- write output to saida.txt
 
 main = do   putStrLn "Main called"
+            readK <- readFile __input_file_k
+            readP <- readFile __input_file_points
+            let dataset = map (read :: Double) (lines readP)
+            let d = findCentroidsFromDataset dataset (read readK :: Int)
+            writeFile __output_file_res "nil"
+            writeFile __output_file_groups "nil"
             return()
