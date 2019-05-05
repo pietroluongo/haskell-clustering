5ba44106c202bdd43f4937e13336630b65ab0d1b
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2019-05-05 00:05:03 -0300

    Initial main implementation

--- Main.hs
+++ Main.hs
@@ -2,12 +2,18 @@ import Algebra.Point
 import Data.List
 import Data.Function
 import Utils
+import Algebra.Group
 
 -- DEBUGGING ---------------------------------------------
 
---__dataset = [[3, 2, 1], [1, 2, 3], [4, 5, 6], [7, 8, 9], [9, 8, 7]]
+__dataset = [[3.0, 2.0, 1.0], [1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0], [9.0, 8.0, 7.0]]
 
 --__dataset_fst_point = getFirstPoint __dataset
+
+__input_file_k = "./testcases/k.txt"
+__input_file_points = "./testcases/entrada.txt"
+__output_file_groups = "./output/saida.txt"
+__output_file_res = "./output/result.txt"
 
 ----------------------------------------------------------
 
@@ -16,3 +22,6 @@ import Utils
 -- do grouping algorithm
 -- write sse value to result.txt
 -- write output to saida.txt
+
+main = do   putStrLn "Main called"
+            return()
