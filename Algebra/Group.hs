672b6a3dcde8c3b2385036d1b4e4771082e139c0
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2019-05-05 18:37:29 -0300

    Added some functions to Algebra.Group
    Added addToGroup and makeEmptyGroup functions.

--- Algebra/Group.hs
+++ Algebra/Group.hs
@@ -1,11 +1,18 @@
 module Algebra.Group (
-    Group(..)
+    Group(..),
+    addToGroup,
+    makeEmptyGroup
 ) where
 
-import Algebra.Point
+import qualified Algebra.Point as AP
 
 data Group = Group {
-    centroid :: Point,
-    points :: [Point]
+    centroid :: AP.Point,
+    points :: [AP.Point]
 } deriving (Show, Eq, Ord)
 
+addToGroup :: AP.Point -> Group -> Group
+addToGroup point group = Group (centroid group) (points (group) ++ [point])
+
+makeEmptyGroup :: AP.Point -> Group
+makeEmptyGroup point = Group point []
