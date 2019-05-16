c0b2d162600b81c6f76e3d3370123d8e843ec079
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2019-05-16 19:53:06 -0300

    WIP: Output formatting
    Almost working.

--- Main.hs
+++ Main.hs
@@ -7,10 +7,6 @@ import System.IO
 import Text.Printf
 -- DEBUGGING ---------------------------------------------
 
-__dataset = [[3.0, 2.0, 1.0], [1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0], [9.0, 8.0, 7.0]]
-
-__dataset_conv = convertDataset __dataset
-
 __input_file_k = "./testcases/k.txt"
 __input_file_points = "./testcases/entrada.txt"
 __output_file_groups = "./output/saida.txt"
@@ -18,25 +14,39 @@ __output_file_res = "./output/result.txt"
 
 __CONST_MAX_IT = 100
 
-__test = [[7.0, 5.4, 6.32, 9.0], [17.0, 32.3, 5.0, 9.99], [33.0, 54.0, 5.6, 65.8], [77.7, 33.4, 98.0, 7.56], [8.9, 5.8, 6.0, 9.0]]
+__test = [[5.1, 3.5, 1.4, 0.2], [4.9, 3.0, 1.4, 0.2], [4.7, 3.2, 1.3, 0.2], [4.6, 3.1, 1.5, 0.2], [5.0, 3.6, 1.4, 0.2], [5.4, 3.9, 1.7, 0.4], [4.6, 3.4, 1.4, 0.3], [5.0, 3.4, 1.5, 0.2], [4.4, 2.9, 1.4, 0.2], [4.9, 3.1, 1.5, 0.1], [5.4, 3.7, 1.5, 0.2], [4.8, 3.4, 1.6, 0.2], [4.8, 3.0, 1.4, 0.1], [4.3, 3.0, 1.1, 0.1], [5.8, 4.0, 1.2, 0.2], [5.7, 4.4, 1.5, 0.4], [5.4, 3.9, 1.3, 0.4], [5.1, 3.5, 1.4, 0.3], [5.7, 3.8, 1.7, 0.3], [5.1, 3.8, 1.5, 0.3], [5.4, 3.4, 1.7, 0.2], [5.1, 3.7, 1.5, 0.4], [4.6, 3.6, 1.0, 0.2], [5.1, 3.3, 1.7, 0.5], [4.8, 3.4, 1.9, 0.2], [5.0, 3.0, 1.6, 0.2], [5.0, 3.4, 1.6, 0.4], [5.2, 3.5, 1.5, 0.2], [5.2, 3.4, 1.4, 0.2], [4.7, 3.2, 1.6, 0.2], [4.8, 3.1, 1.6, 0.2], [5.4, 3.4, 1.5, 0.4], [5.2, 4.1, 1.5, 0.1], [5.5, 4.2, 1.4, 0.2], [4.9, 3.1, 1.5, 0.1], [5.0, 3.2, 1.2, 0.2], [5.5, 3.5, 1.3, 0.2], [4.9, 3.1, 1.5, 0.1], [4.4, 3.0, 1.3, 0.2], [5.1, 3.4, 1.5, 0.2], [5.0, 3.5, 1.3, 0.3], [4.5, 2.3, 1.3, 0.3], [4.4, 3.2, 1.3, 0.2], [5.0, 3.5, 1.6, 0.6], [5.1, 3.8, 1.9, 0.4], [4.8, 3.0, 1.4, 0.3], [5.1, 3.8, 1.6, 0.2], [4.6, 3.2, 1.4, 0.2], [5.3, 3.7, 1.5, 0.2], [5.0, 3.3, 1.4, 0.2], [7.0, 3.2, 4.7, 1.4], [6.4, 3.2, 4.5, 1.5], [6.9, 3.1, 4.9, 1.5], [5.5, 2.3, 4.0, 1.3], [6.5, 2.8, 4.6, 1.5], [5.7, 2.8, 4.5, 1.3], [6.3, 3.3, 4.7, 1.6], [4.9, 2.4, 3.3, 1.0], [6.6, 2.9, 4.6, 1.3], [5.2, 2.7, 3.9, 1.4], [5.0, 2.0, 3.5, 1.0], [5.9, 3.0, 4.2, 1.5], [6.0, 2.2, 4.0, 1.0], [6.1, 2.9, 4.7, 1.4], [5.6, 2.9, 3.6, 1.3], [6.7, 3.1, 4.4, 1.4], [5.6, 3.0, 4.5, 1.5], [5.8, 2.7, 4.1, 1.0], [6.2, 2.2, 4.5, 1.5], [5.6, 2.5, 3.9, 1.1], [5.9, 3.2, 4.8, 1.8], [6.1, 2.8, 4.0, 1.3], [6.3, 2.5, 4.9, 1.5], [6.1, 2.8, 4.7, 1.2], [6.4, 2.9, 4.3, 1.3], [6.6, 3.0, 4.4, 1.4], [6.8, 2.8, 4.8, 1.4], [6.7, 3.0, 5.0, 1.7], [6.0, 2.9, 4.5, 1.5], [5.7, 2.6, 3.5, 1.0], [5.5, 2.4, 3.8, 1.1], [5.5, 2.4, 3.7, 1.0], [5.8, 2.7, 3.9, 1.2], [6.0, 2.7, 5.1, 1.6], [5.4, 3.0, 4.5, 1.5], [6.0, 3.4, 4.5, 1.6], [6.7, 3.1, 4.7, 1.5], [6.3, 2.3, 4.4, 1.3], [5.6, 3.0, 4.1, 1.3], [5.5, 2.5, 4.0, 1.3], [5.5, 2.6, 4.4, 1.2], [6.1, 3.0, 4.6, 1.4], [5.8, 2.6, 4.0, 1.2], [5.0, 2.3, 3.3, 1.0], [5.6, 2.7, 4.2, 1.3], [5.7, 3.0, 4.2, 1.2], [5.7, 2.9, 4.2, 1.3], [6.2, 2.9, 4.3, 1.3], [5.1, 2.5, 3.0, 1.1], [5.7, 2.8, 4.1, 1.3], [6.3, 3.3, 6.0, 2.5], [5.8, 2.7, 5.1, 1.9], [7.1, 3.0, 5.9, 2.1], [6.3, 2.9, 5.6, 1.8], [6.5, 3.0, 5.8, 2.2], [7.6, 3.0, 6.6, 2.1], [4.9, 2.5, 4.5, 1.7], [7.3, 2.9, 6.3, 1.8], [6.7, 2.5, 5.8, 1.8], [7.2, 3.6, 6.1, 2.5], [6.5, 3.2, 5.1, 2.0], [6.4, 2.7, 5.3, 1.9], [6.8, 3.0, 5.5, 2.1], [5.7, 2.5, 5.0, 2.0], [5.8, 2.8, 5.1, 2.4], [6.4, 3.2, 5.3, 2.3], [6.5, 3.0, 5.5, 1.8], [7.7, 3.8, 6.7, 2.2], [7.7, 2.6, 6.9, 2.3], [6.0, 2.2, 5.0, 1.5], [6.9, 3.2, 5.7, 2.3], [5.6, 2.8, 4.9, 2.0], [7.7, 2.8, 6.7, 2.0], [6.3, 2.7, 4.9, 1.8], [6.7, 3.3, 5.7, 2.1], [7.2, 3.2, 6.0, 1.8], [6.2, 2.8, 4.8, 1.8], [6.1, 3.0, 4.9, 1.8], [6.4, 2.8, 5.6, 2.1], [7.2, 3.0, 5.8, 1.6], [7.4, 2.8, 6.1, 1.9], [7.9, 3.8, 6.4, 2.0], [6.4, 2.8, 5.6, 2.2], [6.3, 2.8, 5.1, 1.5], [6.1, 2.6, 5.6, 1.4], [7.7, 3.0, 6.1, 2.3], [6.3, 3.4, 5.6, 2.4], [6.4, 3.1, 5.5, 1.8], [6.0, 3.0, 4.8, 1.8], [6.9, 3.1, 5.4, 2.1], [6.7, 3.1, 5.6, 2.4], [6.9, 3.1, 5.1, 2.3], [6.8, 3.2, 5.9, 2.3], [6.7, 3.3, 5.7, 2.5], [6.7, 3.0, 5.2, 2.3], [6.3, 2.5, 5.0, 1.9], [6.5, 3.0, 5.2, 2.0], [6.2, 3.4, 5.4, 2.3], [5.9, 3.0, 5.1, 1.8]]
 
 __test_conv = convertDataset __test
 
+__centa = Point [2, 2, 2] 1
+
+__centb = Point [4, 4, 4] 2
+
+__b = Point [3, 3, 3] 3
+
+__u = Group __centa [__centa, __centb, __b]
+
+__q = Group __centb [__centb, __centa, __b]
+
+__s = [__u, __q]
+
 ----------------------------------------------------------
 
--- read k.txt
--- read entrada.txt
--- do grouping algorithm
--- write sse value to result.txt
--- write output to saida.txt
 
-
-_dbg = d
+_dbg = b
     where
-        a = findCentroidsFromDataset __test 3
-        b = groupStuff __test_conv a
-        c = group_iter b 0
-        d = getTotalSSE c
+        b = map (map (identifier)) (map (points) __s)
+        --d = intersperse 0 b
+        --a = findCentroidsFromDataset __test 3
+        --b = groupStuff' __test_conv a
+        --c = group_iter b 0
+        --d = getTotalSSE c
+{-
+groupStuff' points centroids = groups
+    where
+    zipped = zip points (map (findNearest' centroids) points)
+    groups = groupPoints zipped
+-}
 
 groupStuff points centroids = groups
     where
@@ -46,7 +56,7 @@ groupStuff points centroids = groups
 group_iter groups cur_iter
     | cur_iter == __CONST_MAX_IT = groups
     | not hasChanged = groups
-    | otherwise = newGroups--group_iter newGroups (cur_iter+1)
+    | otherwise = group_iter newGroups (cur_iter+1)
     where
         pontos = concat $ map (points) groups
         updatedCentroids = recalculateCentroids groups
@@ -59,12 +69,16 @@ recalculateCentroids groups = b
     where
         b = map findCentroid (map (points) groups)
 
-getOutStrings groups = asdf
+getOutStrings groups = fsdf
     where
         asdf = map (map (identifier)) (map (points) groups)
-        --out = getOutStringsRec asdf
+        bsdf = map (map show) asdf
+        csdf = intersperse "\n\n" $ map (unwords) bsdf
+        dsdf = concatenate $ csdf
+        esdf = intersperse "," $ words dsdf
+        fsdf = concatenate esdf
 
-
+concatenate (x:xs) = x++concat xs
 
 main = do   putStrLn "Main called"
             readK <- readFile __input_file_k
@@ -75,8 +89,9 @@ main = do   putStrLn "Main called"
             let conv = convertDataset dataset
             let groupedStuff = groupStuff conv centroids
             let ree = group_iter groupedStuff 0
-            let roo = getOutStrings ree
+            let roo = getOutStrings $ ree
             let sse = getTotalSSE ree
             writeFile __output_file_res $ printf "%.4f" sse
-            writeFile __output_file_groups (show roo)
+            putStrLn $ roo
+            writeFile __output_file_groups (roo)
             return()
